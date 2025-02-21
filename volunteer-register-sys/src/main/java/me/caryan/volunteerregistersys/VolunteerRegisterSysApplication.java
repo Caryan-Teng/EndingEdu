706f81684bde2a6d784577ed2558d3d910a34069
Caryan <15716273511@163.com>
2025-02-21 12:37:46 +0800

    maven初始化

---
+++ volunteer-register-sys/src/main/java/me/caryan/volunteerregistersys/VolunteerRegisterSysApplication.java
@@ -0,0 +1,13 @@
+package me.caryan.volunteerregistersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerRegisterSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerRegisterSysApplication.class, args);
+    }
+
+}
---
+++ volunteer-register-sys/src/test/java/me/caryan/volunteerregistersys/VolunteerRegisterSysApplicationTests.java
@@ -0,0 +1,13 @@
+package me.caryan.volunteerregistersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerRegisterSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
